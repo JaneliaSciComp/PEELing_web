11894164222dc727daa18b3029c95c1c35ecd273
Jody Clements <jody@neomorphic.co.uk>
2023-05-19 16:45:06 -0400

    Updates UserInput to accept cellular compartment...
    and custom true positive & false positive files when using a cellular
    compartment that has not been preselected.

--- client/src/UserInput.js
+++ client/src/UserInput.js
@@ -12,7 +12,16 @@ export default class UserInput extends React.Component {
             tolerance:0,
             plotFormat: 'png',
             plotFormatList: ['png'],
+            cellCompartment: 'cs',
+            cellCompartmentList: [
+              {label: 'Cell Surface', value: 'cs'},
+              {label: 'Mitochondrion', value: 'mt'},
+              {label: 'Nucleus', value: 'nu'},
+              {label: 'Other (Custom TP / FP lists)', value: 'ot'}
+            ],
             fileInvalid:false,
+            fpFileInvalid:false,
+            tpFileInvalid:false,
             controlsInvalid:false,
             replicatesInvalid:false,
             conditionsInvalid:false,
@@ -21,17 +30,20 @@ export default class UserInput extends React.Component {
 
         this.submit = this.submit.bind(this);
         this.fileSelectHandler = this.fileSelectHandler.bind(this);
+        this.tpFileHandler = this.tpFileHandler.bind(this);
+        this.fpFileHandler = this.fpFileHandler.bind(this);
         this.numberChangeHandler = this.numberChangeHandler.bind(this);
         this.formatChangeHandler = this.formatChangeHandler.bind(this);
+        this.cellCompartmentHandler = this.cellCompartmentHandler.bind(this);
     }
 
 
     componentDidMount() {
-        fetch("/api/format", { 
+        fetch("/api/format", {
             method: 'GET'
         }).then(res => {
             if (res.ok) {
-                return res.json(); 
+                return res.json();
             } else {
                 this.props.setError(res.statusText);
             }
@@ -46,7 +58,7 @@ export default class UserInput extends React.Component {
     }
 
 
-    submit(e) { 
+    submit(e) {
         e.preventDefault();
         this.props.submitIndicator(true);
         this.props.setResultsId(null);
@@ -91,7 +103,7 @@ export default class UserInput extends React.Component {
         //this.setState({fileInvalid:true});
         if (e.target.files.length !== 0 && e.target.files[0].type === 'text/tab-separated-values') {
             //let totalInvalid = false || this.state.controlsInvalid || this.state.replicatesInvalid || this.state.conditionsInvalid || this.state.toleranceInvalid;
-            this.setState({fileInvalid: false, 
+            this.setState({fileInvalid: false,
                            //invalid: totalInvalid
                         });
         } else {
@@ -99,6 +111,22 @@ export default class UserInput extends React.Component {
             this.setState({fileInvalid: true,
                            //invalid: totalInvalid
                         });
+        }
+    }
+
+    tpFileHandler(e) {
+        if (e.target.files.length !== 0 && e.target.files[0].type === 'text/tab-separated-values') {
+            this.setState({tpFileInvalid: false});
+        } else {
+            this.setState({tpFileInvalid: true});
+        }
+    }
+
+    fpFileHandler(e) {
+        if (e.target.files.length !== 0 && e.target.files[0].type === 'text/tab-separated-values') {
+            this.setState({fpFileInvalid: false});
+        } else {
+            this.setState({fpFileInvalid: true});
         }
     }
 
@@ -116,11 +144,11 @@ export default class UserInput extends React.Component {
                             });
             }
         }
-        
+
         if (id === 'replicates') {
             if (value && Number.isInteger(value) && value>0) {
                 this.setState({replicates: value,
-                               replicatesInvalid: false, 
+                               replicatesInvalid: false,
                             });
             } else {
                 this.setState({replicatesInvalid: true,
@@ -131,7 +159,7 @@ export default class UserInput extends React.Component {
         if (id === 'tolerance') {
             if ((value===0 || value) && Number.isInteger(value) && value>=0 && value < this.state.controls*this.state.replicates) {
                 this.setState({tolerance: value,
-                               toleranceInvalid: false, 
+                               toleranceInvalid: false,
                             });
             } else {
                 this.setState({toleranceInvalid: true,
@@ -148,20 +176,58 @@ export default class UserInput extends React.Component {
         )
     }
 
+    cellCompartmentHandler(e) {
+      this.setState({
+        cellCompartment: e.target.value
+      });
+    }
+
 
     render() {
         return (
-          <div className='section'>  
-            
+          <div className='section'>
+
             <h4 className='section-title my-4 px-3'>User Input</h4>
-            
+
             <div className='section-main' id='user-input-container'>
                 <Form className='mx-3' onSubmit={this.submit} encType='multipart/form-data'>
+                    <Form.Group as={Row} className='mt-4 px-3' controlId='plot_format'>
+                        <Form.Label column sm={5}>Cellular Compartment</Form.Label>
+                        <Col sm={7}>
+                            <Form.Select name='cellular_compartment' value={this.state.cellCompartment} onChange={this.cellCompartmentHandler}>
+                            {this.state.cellCompartmentList.map((format, i) =>
+                             <option key={i} value={format.value} >{format.label}</option>
+                            )}
+                            </Form.Select>
+                        </Col>
+                    </Form.Group>
+                    { this.state.cellCompartment === 'ot' ? (
+                      <>
+                      <Form.Group as={Row} className='mt-4 px-3'>
+                        <Form.Label column sm={5}>TP Reference (.tsv)</Form.Label>
+                        <Col sm={7}>
+                            <Form.Control type='file' name='tp_file' required accept='.tsv' onChange={this.tpFileHandler}/>
+                            {this.state.tpFileInvalid ?
+                            <p className='invalid' display='none'>Please select a tsv file.</p>
+                            : null}
+                        </Col>
+                        </Form.Group>
+                      <Form.Group as={Row} className='mt-4 px-3'>
+                        <Form.Label column sm={5}>FP Reference (.tsv)</Form.Label>
+                         <Col sm={7}>
+                            <Form.Control type='file' name='fp_file' required accept='.tsv' onChange={this.fpFileHandler}/>
+                            {this.state.fpFleInvalid ?
+                            <p className='invalid' display='none'>Please select a tsv file.</p>
+                            : null}
+                        </Col>
+                      </Form.Group>
+                      </>) : null}
+
                     <Form.Group as={Row} className='mt-4 px-3'>
                         <Form.Label column sm={5}>Data File (.tsv)</Form.Label>
                         <Col sm={7}>
                             <Form.Control type='file' name='mass_file' required accept='.tsv' onChange={this.fileSelectHandler}/>
-                            {this.state.fileInvalid ? 
+                            {this.state.fileInvalid ?
                             <p className='invalid' display='none'>Please select a tsv file.</p>
                             : null}
                         </Col>
@@ -172,7 +238,7 @@ export default class UserInput extends React.Component {
                         <Col sm={7}>
                             <Form.Control type='number' name='controls' required min='1' onChange={this.numberChangeHandler}/>
                             <Form.Control.Feedback type='invalid'>Please provide a positive integer.</Form.Control.Feedback>
-                            {this.state.controlsInvalid ? 
+                            {this.state.controlsInvalid ?
                             <p className='invalid' display='none'>Please provide a positive integer.</p>
                             : null}
                         </Col>
@@ -183,7 +249,7 @@ export default class UserInput extends React.Component {
                         <Col sm={7}>
                             <Form.Control type='number' name='replicates' required min='1' onChange={this.numberChangeHandler}/>
                             <Form.Control.Feedback type='invalid'>Please provide a positive integer.</Form.Control.Feedback>
-                            {this.state.replicatesInvalid ? 
+                            {this.state.replicatesInvalid ?
                             <p className='invalid' display='none'>Please provide a positive integer.</p>
                             : null}
                         </Col>
@@ -194,7 +260,7 @@ export default class UserInput extends React.Component {
                         <Col sm={7}>
                             <Form.Control type='number' name='tolerance' placeholder='0' min='0' max={this.state.controls*this.state.replicates} onChange={this.numberChangeHandler}/>
                             <Form.Control.Feedback type='invalid'>Please provide a non-negative integer with value ≤ controls * replicates * conditions.</Form.Control.Feedback>
-                            {this.state.toleranceInvalid ? 
+                            {this.state.toleranceInvalid ?
                             <p className='invalid' display='none'>Please provide an integer in [0, #controls * #replicates).</p>
                             : null}
                         </Col>
@@ -205,7 +271,7 @@ export default class UserInput extends React.Component {
                         <Col sm={7}>
                             <Form.Select name='plot_format' value={this.state.plotFormat} onChange={this.formatChangeHandler}>
                             {this.state.plotFormatList.map((format, i) =>
-                             <option key={i} value={format} >{format}</option> 
+                             <option key={i} value={format} >{format}</option>
                             )}
                             </Form.Select>
                         </Col>
@@ -214,7 +280,7 @@ export default class UserInput extends React.Component {
                     <Row className='my-4 px-3 flex-row-reverse'>
                         <Button type='submit' className='btn-submit' onSubmit={this.submit}>Submit</Button>
                     </Row>
-                    
+
                 </Form>
             </div>
           </div>
